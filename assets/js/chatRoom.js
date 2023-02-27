c7c1360e0d75b89a63f4ff895bf5b02828ff426a
0empty <2zeroempty@gmail.com>
2023-02-27 21:52:20 +0900

    #32 <fix>: sender 프로필에서 받아오게 수정

--- assets/js/chatRoom.js
+++ assets/js/chatRoom.js
@@ -1,7 +1,7 @@
 // 토큰 불러오기
 $(document).ready(function () {
   const ok = localStorage.getItem('accessToken')
-
+  getProfile(); 
   if (ok.length == 0) {
     console.error("userId 요소를 찾을 수 없습니다.");
     return;
@@ -12,6 +12,7 @@ $(document).ready(function () {
 
 const userToken = localStorage.getItem('accessToken')
 // let 변경 가능, const 변경불가능; -> 개발자
+
 
 
 
@@ -75,7 +76,7 @@ function chatList() {
     dataType: 'json',
     success: function (response) {
       // 가져온 데이터로 채팅 리스트를 렌더링합니다.
-      let roomList = response.roomList;
+      let roomList = response;
 
       // 가져온 채팅방 리스트를 동적으로 추가합니다.
       for (let i = 0; i < roomList.length; i++) {
@@ -126,7 +127,7 @@ function chatList() {
 // 전역 변수로 현재 방의 정보 저장 -> 두번 클릭 시 계속해서 같은 내용 붙어서 불러오는 이슈 해결
 let currentRoomId = null;
 let currentNickname = null;
-
+var sender = "";
 
 
 function chatView(roomId, nickname, producId) {
@@ -267,8 +268,8 @@ function connect(roomId, nickname, productId) {
 };
 
 
-function sendChat(roomId, productId, nickname, sender) {
-  console.log(roomId, productId, nickname, sender)
+function sendChat(roomId, productId, nickname) {
+  console.log(roomId, productId, nickname)
   stompClient.send("/pub/send", { Authorization: userToken }, JSON.stringify({
     "message": $("#message").val(),
     "receiver": nickname,
@@ -314,4 +315,42 @@ function deleteChat(roomId) {
       window.location.reload();
     },
   });
+}
+
+function getProfile(){
+  var settings = {
+      "url": "http://localhost:8080/users/profile",
+      "method": "GET",
+      "timeout": 0,
+      "headers": {
+        "Authorization": localStorage.getItem('accessToken')
+      },
+    };
+    $.ajax(settings).done(function (response) {
+      const nickname = response.nickname;
+      sender = nickname;
+      // $('#loginForm').siblings('span.nickName').text(response.nickname + "님").parent('.loginForm').addClass('hasNickname');
+      document.getElementById('loginbuttons').style.display = 'none';
+      let temp_html = `<li class="dropdown dropdown-large" style="margin-top: 13px; margin-right: 10px">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown" style = "color:black">${nickname}님 <b class="caret"></b></a>
+      
+      <ul class="dropdown-menu dropdown-menu-end" >
+          <li class="col-sm-6">
+              <ul>
+                  <li class="dropdown-header">${nickname}님</li>
+                  <li><a href="myinfo.html">내정보</a></li>
+                  <li><a href="#">판매상품</a></li>
+                  <li><a href="#">구매상품</a></li>
+                  <li><a href="chatroom.html">채팅</a></li>
+                  <li><a href="myinterest.html">관심목록</a></li>
+              </ul>
+          </li>               
+      </ul>      
+  </li>
+  <div style = "color:#82ca9c; margin-left 10px; margin-top: 14px" ><a onclick = "logout()" > 로그아웃 </a></div>`
+  $('#loginForm').append(temp_html)
+      element.innerHTML = '<div style = "color:#82ca9c; margin-top: 14px" ><a onclick = "logout()" > 로그아웃 </a></div>';
+    }).fail(function(){
+      reissueToken();
+});
 }
